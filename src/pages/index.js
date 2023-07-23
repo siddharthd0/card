154f027f4c40f7645409858f2ba4ffdbccaa1000
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-07-23 00:37:47 -0600

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,77 +1,195 @@
 import {
   HStack,
   Text,
+  Tooltip,
+  Flex,
   Box,
   Button,
+  Link,
+  Image,
   Heading,
   VStack,
   useColorMode,
+  IconButton,
 } from "@chakra-ui/react";
 import Head from "next/head";
-import { FaOptinMonster, FaGithub, FaDiscord } from "react-icons/fa";
+import {
+  FaOptinMonster,
+  FaGithub,
+  FaDiscord,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedin,
+  FaTiktok,
+} from "react-icons/fa";
 
-// LinkButton Component
+const SocialMediaIcons = () => (
+  <>
+    <HStack spacing={3}>
+      <Tooltip label="Twitter" borderRadius="md" bg="gray.800">
+        <Link
+          _hover={{
+            color: "twitter.500 !important",
+          }}
+          href="https://twitter.com/siddharthd01"
+          isExternal
+          color="whiteAlpha.500"
+        >
+          <FaTwitter size={20} />
+        </Link>
+      </Tooltip>
+      <Tooltip label="Instagram" borderRadius="md" bg="gray.800">
+        <Link
+          _hover={{
+            color: "red.500 !important",
+          }}
+          href="https://www.instagram.com/siddharth.duggal/"
+          isExternal
+          color="whiteAlpha.500"
+        >
+          <FaInstagram size={20} />
+        </Link>
+      </Tooltip>
+
+      <Tooltip label="LinkedIn" borderRadius="md" bg="gray.800">
+        <Link
+          _hover={{
+            color: "linkedin.500 !important",
+          }}
+          href="https://www.linkedin.com/in/siddharth-duggal/"
+          isExternal
+          color="whiteAlpha.500"
+        >
+          <FaLinkedin size={20} />
+        </Link>
+      </Tooltip>
+      <Tooltip label="GitHub" borderRadius="md" bg="gray.800">
+        <Link
+          _hover={{
+            color: "gray.200 !important",
+          }}
+          href="https://github.com/siddharthd0"
+          isExternal
+          color="whiteAlpha.500"
+        >
+          <FaGithub size={20} />
+        </Link>
+      </Tooltip>
+    </HStack>
+  </>
+);
 const LinkButton = ({ name, url, icon }) => {
   return (
-    <Button
-      colorScheme="white"
-      variant={"outline"}
-      size="lg"
-      as="a"
+    <Link
+      _hover={{
+        textDecoration: "none",
+      }}
       href={url}
-      w="300px"
+      isExternal
     >
-      <HStack justifyContent="center" width="full">
-        {icon}
-        <Text textAlign={"center"}>{name}</Text>
-      </HStack>
-    </Button>
+      <Button
+        transition={"all .2s ease-in-out"}
+        bg="transparent"
+        border="1px solid"
+        borderColor="whiteAlpha.400"
+        color="whiteAlpha.900"
+        _hover={{
+          color: "black",
+          bg: "white",
+        }}
+        size="lg"
+        w="350px"
+      >
+        <HStack justifyContent="center" width="full">
+          {icon}
+          <Text ml="6px" textAlign={"center"}>
+            {name}
+          </Text>
+        </HStack>
+      </Button>
+    </Link>
   );
 };
 
 export default function Home() {
   const { colorMode } = useColorMode();
 
-  const bgColor = { light: "blackAlpha.900", dark: "gray.800" };
+  const bgColor = { light: "#020E69", dark: "gray.800" };
   const color = { light: "white", dark: "white" };
 
   return (
-    <Box
-      minHeight="100vh"
-      bg={bgColor[colorMode]}
-      color={color[colorMode]}
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Head>
-        <title> Siddharth Duggal</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Box
+        minHeight="100vh"
+        bg={"rgb(22, 22, 22) "}
+        color={color[colorMode]}
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Head>
+          <title>Links | Siddharth Duggal</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Flex mb={8} alignItems="center">
+          <Image
+            transition={"all .2s ease-in-out"}
+            _hover={{
+              transform: "scale(1.1)",
+            }}
+            borderRadius="full"
+            boxSize="70px"
+            src="siddharth.jpeg"
+            alt="Siddharth Duggal"
+          />
+          <Box ml="1rem">
+            <Tooltip
+              bg={"black"}
+              mb="1rem"
+              placement="top"
+              label="View my portfolio website"
+            >
+              <Link
+                _hover={{
+                  textDecoration: "none",
+                }}
+                href="https://siddharthduggal.com"
+              >
+                <Heading
+                  mb={1}
+                  transition={"all .2s ease-in-out"}
+                  _hover={{
+                    color: "whiteAlpha.700",
+                  }}
+                  fontWeight={"400"}
+                >
+                  Siddharth Duggal
+                </Heading>
+              </Link>
+            </Tooltip>
+            <SocialMediaIcons />
+          </Box>
+        </Flex>
 
-      <Heading fontWeight={"500"} size="2xl" mb={8}>
-        Siddharth Duggal
-      </Heading>
-
-      <VStack spacing={5}>
-        <LinkButton
-          name="Free Coding Courses"
-          url="https://www.techoptimum.org"
-          icon={<FaOptinMonster />}
-        />
-        <LinkButton
-          name="2024 SWE Internships"
-          url="https://github.com/TechOptimum/2024-swe-internships"
-          icon={<FaGithub />}
-        />
-        <LinkButton
-          name="Coding Community"
-          url="https://discord.gg/HpRfm7kp3U"
-          icon={<FaDiscord />}
-        />
-        {/* Add more LinkButtons as needed */}
-      </VStack>
-    </Box>
+        <VStack spacing={5}>
+          <LinkButton
+            name="Free Coding Courses"
+            url="https://www.techoptimum.org"
+            icon={<FaOptinMonster />}
+          />
+          <LinkButton
+            name="2024 SWE Internships"
+            url="https://github.com/TechOptimum/2024-swe-internships"
+            icon={<FaGithub />}
+          />
+          <LinkButton
+            name="Coding Community"
+            url="https://discord.gg/HpRfm7kp3U"
+            icon={<FaDiscord />}
+          />
+        </VStack>
+      </Box>
+    </>
   );
 }
