afda8fa1b737b7d69638e70407d740604ef8340b
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-08-25 04:23:26 -0600

    push

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,12 @@
 import '@/styles/globals.css'
 import * as React from 'react'
 import { ChakraProvider } from "@chakra-ui/react"
-
+import { Analytics } from '@vercel/analytics/react';
 export default function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   )
 }
